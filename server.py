57d9f531f2926ebc6ef401a02d9447738ed02635
Brian Warner <warner@lothar.com>
2013-05-15 14:03:24 -0700

    added pairing flow

--- server.py
+++ server.py
@@ -13,7 +13,25 @@ def api():
     body = request.json["body"]
     if verb == "email":
         print "Email", body["email"]
-    return jsonify(ok="ok", known=True)
+        known = False
+        if body["email"] == "known":
+            known = True
+        return jsonify(ok="ok", known=known)
+    if verb == "attach":
+        if body["password"] == "password":
+            return jsonify(correct=True)
+        return jsonify(correct=False)
+    if verb == "create-account":
+        print "create-account", body["password"]
+        return jsonify()
+    if verb == "got-code":
+        if body["code"] == "123-456-7890":
+            return jsonify(correct=True)
+        return jsonify(correct=False)
+    if verb == "reset-account":
+        print "reset-account", body["password"]
+        return jsonify(ok=True)
+    return jsonify(error="unknown verb")
 
 if __name__ == '__main__':
     app.debug = True
--- static/main.js
+++ static/main.js
@@ -51,9 +51,9 @@ setupFunctions["t1-get-email"] = function() {
 };
 
 setupFunctions["t2-get-password"] = function() {
-    $("#dialog input.password").on("click", function() {
+    $("#dialog input.go").on("click", function() {
         var password = $("#dialog input.password").val();
-        send("password", {password: password})
+        send("attach", {password: password})
             .then(function(r) {
                 if (r.correct)
                     switchTo("t4-password-good");
@@ -63,16 +63,113 @@ setupFunctions["t2-get-password"] = function() {
                     
     });
 
-    $("#dialog input.forgot").on("click", function() {
-        switchTo("t6-forgot-password-start");
-    });
-
     $("#dialog input.hate").on("click", function() {
         switchTo("t7-hate-password");
     });
 
 };
-    
+
+setupFunctions["t3-create-account"] = function() {
+    $("#dialog input.go").on("click", function() {
+        var password = $("#dialog input.password").val();
+        send("create-account", {password: password})
+            .then(function(r) {
+                switchTo("t4a-account-created");
+            });
+                    
+    });
+
+    var randomPW = "correct-horse-battery-staple";
+    $("#dialog input.generated-password").attr("value", randomPW);
+    $("#dialog input.generated-password").on("click", function() {
+        $("#dialog input.password").val(randomPW);
+    });
+
+};
+
+setupFunctions["t5-password-wrong"] = function() {
+    $("#dialog input.again").on("click", function() {
+        switchTo("t2-get-password");
+    });
+}; 
+
+setupFunctions["t7-hate-password"] = function() {
+    $("#dialog input.pair").on("click", function() {
+        switchTo("t12-pair-start");
+    });
+    $("#dialog input.reset").on("click", function() {
+        switchTo("t8-reset-account-start");
+    });
+};
+
+setupFunctions["t8-reset-account-start"] = function() {
+    $("#dialog span.email").text(state.email);
+    var code = "123-456-7890";
+    $("#dialog span.code").text(code);
+    $("#dialog input.go").on("click", function() {
+        var gotCode = $("#dialog input.code").val();
+        $("#dialog input.code").hide(); // show "checking.." message
+        send("got-code", {code: gotCode})
+            .then(function(r) {
+                if (r.correct)
+                    switchTo("t9-reset-account-set-password");
+                else
+                    switchTo("t8a-reset-account-bad-code");
+            });
+    });
+};
+
+setupFunctions["t8a-reset-account-bad-code"] = function() {
+    $("#dialog input.again").on("click", function() {
+        switchTo("t8-reset-account-start");
+    });
+}; 
+
+setupFunctions["t9-reset-account-set-password"] = function() {
+    $("#dialog input.go").on("click", function() {
+        state.newPassword = $("#dialog input.password").val();
+        switchTo("t10-reset-account-commit");
+    });
+
+    var randomPW = "correct-horse-battery-staple";
+    $("#dialog input.generated-password").attr("value", randomPW);
+    $("#dialog input.generated-password").on("click", function() {
+        $("#dialog input.password").val(randomPW);
+    });
+};
+
+setupFunctions["t10-reset-account-commit"] = function() {
+    $("#dialog input.reset").on("click", function() {
+        send("reset-account", {password: state.newPassword})
+            .then(function(r) {
+                switchTo("t11-reset-account-done");
+            });
+                    
+    });
+};
+
+setupFunctions["t12-pair-start"] = function() {
+    $("#dialog input.ok").on("click", function() {
+        switchTo("t13-pair-more");
+    });
+};
+
+setupFunctions["t13-pair-more"] = function() {
+    $("#dialog div.code").text("123-456");
+    $("#dialog input.ok").on("click", function() {
+        switchTo("t14-pair-done");
+    });
+    $("#dialog input.wrong").on("click", function() {
+        switchTo("t15-pair-wrong");
+    });
+};
+
+setupFunctions["t15-pair-wrong"] = function() {
+    $("#dialog input.again").on("click", function() {
+        switchTo("t12-pair-start");
+    });
+};
+   
 
 $(function() {
     console.log("starting");
